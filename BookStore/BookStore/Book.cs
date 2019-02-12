edf8b25a2f1b541382902ebc94b12dfe2842aa0e
Ramiz Hanan <ramizhanan@gmail.com>
2019-02-11 18:39:41 -0800

    Created 3 dummy objects
    -defautlt constructor
    -overloaded constructor

---
+++ BookStore/BookStore/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore
+{
+    class Book
+    {
+        public const decimal tax = .08m;
+
+        string author = "";
+        string price = "";
+        string ISBN = "";
+        string bookName = "";
+
+        public Book(string BookName, string Author, string isbn, string Price)
+        {
+            author = Author;
+            price = Price;
+            ISBN = isbn;
+            bookName = BookName;
+        }
+
+        public Book() {
+            author = null;
+            price = null;
+            ISBN = null;
+            bookName = null;
+        }
+
+    }
+    
+}
--- BookStore/BookStore/Program.cs
+++ BookStore/BookStore/Program.cs
@@ -8,15 +8,37 @@ namespace BookStore
 {
     static class Program
     {
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            string Book1Name = "";
+            string Book1Price = "";
+            string Book1ISBN = "";
+            string Book1Author = "";
+
+            string Book2Name = "";
+            string Book2Price = "";
+            string Book2ISBN = "";
+            string Book2Author = "";
+
+            string Book3Name = "";
+            string Book3Price = "";
+            string Book3ISBN = "";
+            string Book3Author = "";
+
+            Book Book1 = new Book(Book1Name, Book1Author, Book1ISBN, Book1Price);
+            Book Book2 = new Book(Book2Name, Book2Author, Book2ISBN, Book2Price);
+            Book Book3 = new Book(Book3Name, Book3Author, Book3ISBN, Book3Price);
+
+
         }
     }
 }
